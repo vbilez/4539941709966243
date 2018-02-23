b740b00ce4bc889b55ac44d31ba510c033a41a29
vbilez <d.sqate@gmail.com>
2018-02-23 16:05:59 +0200

    add youtube, mp4 video types

--- wp-content/themes/liberty/video.php
+++ wp-content/themes/liberty/video.php
@@ -4,13 +4,33 @@
             $post=get_post($id);
 $content=$post->post_content;
 $title=$post->post_title;
+$youtubelink='youtubelink';
+$vimeolink='vimeolink';
+$postname=$post->post_name;
+if(
+    (strpos($postname,$youtubelink)===false)
+     &&
+    (strpos($postname,$vimeolink)===false)
+  )
+{
+  $video_type="video/mp4";
+}
+if(strpos($postname,$youtubelink)!==false)
+{
+  $video_type="video/youtube";
+}
+if(strpos($postname,$vimeolink)!==false)
+{
+  $video_type="video/vimeo";
+}
+
 
 $postdate= get_the_date('d',$post) .'|'.get_the_date('m',$post) ;
 echo '
 <div class="col-md-7  col-lg-7 col-sm-12 col-xs-12 videoplayer" style="padding-left:0 !important;padding-right:0 !important" >
 			<video  style="width: 100%; height: 100%;" id="my-video" class="video-js vjs-big-play-centered vjs-16-9" controls preload="auto" 
   poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
-    <source src="'.get_the_guid($id).'" type="video/youtube">
+    <source src="'.get_the_guid($id).'" type="'.$video_type.'">
     
     <p class="vjs-no-js">
       To view this video please enable JavaScript, and consider upgrading to a web browser that
