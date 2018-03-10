8a1b18888e5e6dfdfd80ebfff4ec615cb4a7f13c
Iterl0 <Iterl054@gmail.com>
2018-03-10 17:43:02 +0200

    adjust responsiveness for tablet on portfolio wedding

--- wp-content/themes/liberty/portfolio.php
+++ wp-content/themes/liberty/portfolio.php
@@ -84,153 +84,163 @@ $q = new WP_Query( $args );
 $q2 = new WP_Query( $args2 );
 ?>
    
-<div class="video-thumbs-table col-xs-12">
-    <?php if(($currentSegment=='portfolio-wedding')||($currentSegment=='portfolio-production')){?>
-    <div class="prev-video-thumbs-arrow">
-        <?php previous_posts_link('<i style="color:#66FAF9" class="fa fa-4x fa-chevron-left"></i>',$q->max_num_pages);?>
-    </div>
-    <div class="next-video-thumbs-arrow">
-        <?php next_posts_link('<i style="color:#66FAF9" class="fa fa-4x fa-chevron-right"></i>',$q->max_num_pages);?>
-    </div>
-    <?php }?>
 <div class="row">
+    <div class="video-thumbs-table col-xs-12">
+        <?php if(($currentSegment=='portfolio-wedding')||($currentSegment=='portfolio-production')){?>
+            <div class="prev-video-thumbs-arrow">
+                <?php previous_posts_link('<i style="color:#66FAF9" class="fa fa-4x fa-chevron-left"></i>',$q->max_num_pages);?>
+            </div>
+            <div class="next-video-thumbs-arrow">
+                <?php next_posts_link('<i style="color:#66FAF9" class="fa fa-4x fa-chevron-right"></i>',$q->max_num_pages);?>
+            </div>
+        <?php }?>
+        <div class="row">
 
-<?php
-/**
- * Like get_template_part() put lets you pass args to the template file
- * Args are available in the tempalte as $template_args array
- * @param string filepart
- * @param mixed wp_args style argument list
- */
-function hm_get_template_part( $file, $template_args = array(), $cache_args = array() ) {
-    $template_args = wp_parse_args( $template_args );
-    $cache_args = wp_parse_args( $cache_args );
-    if ( $cache_args ) {
-        foreach ( $template_args as $key => $value ) {
-            if ( is_scalar( $value ) || is_array( $value ) ) {
-                $cache_args[$key] = $value;
-            } else if ( is_object( $value ) && method_exists( $value, 'get_id' ) ) {
-                $cache_args[$key] = call_user_method( 'get_id', $value );
+            <?php
+            /**
+             * Like get_template_part() put lets you pass args to the template file
+             * Args are available in the tempalte as $template_args array
+             * @param string filepart
+             * @param mixed wp_args style argument list
+             */
+            function hm_get_template_part( $file, $template_args = array(), $cache_args = array() ) {
+                $template_args = wp_parse_args( $template_args );
+                $cache_args = wp_parse_args( $cache_args );
+                if ( $cache_args ) {
+                    foreach ( $template_args as $key => $value ) {
+                        if ( is_scalar( $value ) || is_array( $value ) ) {
+                            $cache_args[$key] = $value;
+                        } else if ( is_object( $value ) && method_exists( $value, 'get_id' ) ) {
+                            $cache_args[$key] = call_user_method( 'get_id', $value );
+                        }
+                    }
+                    if ( ( $cache = wp_cache_get( $file, serialize( $cache_args ) ) ) !== false ) {
+                        if ( ! empty( $template_args['return'] ) )
+                            return $cache;
+                        echo $cache;
+                        return;
+                    }
+                }
+                $file_handle = $file;
+                do_action( 'start_operation', 'hm_template_part::' . $file_handle );
+                if ( file_exists( get_stylesheet_directory() . '/' . $file . '.php' ) )
+                    $file = get_stylesheet_directory() . '/' . $file . '.php';
+                elseif ( file_exists( get_template_directory() . '/' . $file . '.php' ) )
+                    $file = get_template_directory() . '/' . $file . '.php';
+                ob_start();
+                $return = require( $file );
+                $data = ob_get_clean();
+                do_action( 'end_operation', 'hm_template_part::' . $file_handle );
+                if ( $cache_args ) {
+                    wp_cache_set( $file, $data, serialize( $cache_args ), 3600 );
+                }
+                if ( ! empty( $template_args['return'] ) )
+                    if ( $return === false )
+                        return false;
+                    else
+                        return $data;
+                echo $data;
             }
-        }
-        if ( ( $cache = wp_cache_get( $file, serialize( $cache_args ) ) ) !== false ) {
-            if ( ! empty( $template_args['return'] ) )
-                return $cache;
-            echo $cache;
-            return;
-        }
-    }
-    $file_handle = $file;
-    do_action( 'start_operation', 'hm_template_part::' . $file_handle );
-    if ( file_exists( get_stylesheet_directory() . '/' . $file . '.php' ) )
-        $file = get_stylesheet_directory() . '/' . $file . '.php';
-    elseif ( file_exists( get_template_directory() . '/' . $file . '.php' ) )
-        $file = get_template_directory() . '/' . $file . '.php';
-    ob_start();
-    $return = require( $file );
-    $data = ob_get_clean();
-    do_action( 'end_operation', 'hm_template_part::' . $file_handle );
-    if ( $cache_args ) {
-        wp_cache_set( $file, $data, serialize( $cache_args ), 3600 );
-    }
-    if ( ! empty( $template_args['return'] ) )
-        if ( $return === false )
-            return false;
-        else
-            return $data;
-    echo $data;
-}
 
 
-if($currentSegment=='portfolio-wedding' || $currentSegment=='portfolio-production' || $currentSegment=='thankyou'
-    || $currentSegment=='video')
-{
-    if($currentSegment=='portfolio-wedding')   {
+            if($currentSegment=='portfolio-wedding' || $currentSegment=='portfolio-production' || $currentSegment=='thankyou'
+                || $currentSegment=='video')
+            {
+                if($currentSegment=='portfolio-wedding')   {
 
-        if ( $q->have_posts() ) {
-            while ( $q->have_posts() ) {
-                $q->the_post();
-                // );
-                $category=get_the_category();
-                $category_use=$category[0]->term_id;
-                $postdate=   get_the_date('d') .'|'.get_the_date('m') ;
-                $post_title=mb_strimwidth(get_the_title(),0,15); 
-                $post_thumbnail=get_the_post_thumbnail_url(get_the_ID(),'post_thumbnail');
-                echo'
-                <div class="col-md-6 col-xs-12 col-sm-6 col-lg-4">
+                    if ( $q->have_posts() ) {
+                        while ( $q->have_posts() ) {
+                            $q->the_post();
+                            // );
+                            $category=get_the_category();
+                            $category_use=$category[0]->term_id;
+                            $postdate=   get_the_date('d') .'|'.get_the_date('m') ;
+                            $post_title=mb_strimwidth(get_the_title(),0,15);
+                            $post_thumbnail=get_the_post_thumbnail_url(get_the_ID(),'post_thumbnail');
+                            echo'
+                <div class="col-md-4 col-xs-12 col-sm-6">
                 <a href='.get_home_url().'/video/'.get_the_ID() .'/'.$category_use.'">'.'
                 <img src="'.$post_thumbnail.'">
                 '.'</a>'.'
-            <div class="video-thumb-caption-box">
-                <div class="video-thumb-title-text" >'.$post_title .'</div>
-                <div class="video-thumb-title-date" >'.$postdate.'</div>
-            </div>
-        </div>';
-                // Your loop
+                <div class="video-thumb-caption-box">
+                    <div class="video-thumb-title-text" >'.$post_title .'</div>
+                    <div class="video-thumb-title-date" >'.$postdate.'</div>
+                </div>
+                </div>';
+                            // Your loop
+
+                        }
+                        wp_reset_postdata();
+
+                    }
+
+                    //get_template_part( 'portfolio-wedding' );
+                }
+                if($currentSegment=='portfolio-production')   {
+                    if ( $q2->have_posts() ) {
+                        while ( $q2->have_posts() ) {
+                            $q2->the_post();
+                            // );
+                            $category=get_the_category();
+                            $category_use=$category[0]->term_id;
+                            $postdate=   get_the_date('d') .'|'.get_the_date('m') ;
+                            $post_title=mb_strimwidth(get_the_title(),0,15);
+                            $post_thumbnail=get_the_post_thumbnail_url(get_the_ID(),'post_thumbnail');
+                            echo'
+                <div class="col-md-4 col-xs-12 col-sm-12">
+                    <a href='.get_home_url().'/video/'.get_the_ID() .'/'.$category_use.'">'.'
+                        <img src="'.$post_thumbnail.'">
+                    '.'</a>'.'
+                    <div class="video-thumb-caption-box"">
+                      <div class="video-thumb-title-text" >'.$post_title .'</div>
+                      <div class="video-thumb-title-date" >'.$postdate.'</div>
+                    </div>
+                </div>';
+                            // Your loop
+
+                        }
+                        wp_reset_postdata();
+                    }
+                    //get_template_part( 'portfolio-production' );
+                }
+                if($currentSegment=='video')   {
+                    //get_template_part( 'video' );
+                    hm_get_template_part( 'video',['option'=>$idseg]);
+                }
+                if($currentSegment=='thankyou')   {
+                    //$ty=1;$mailcontent=   get_template_part( 'content' );
+                }
+            }
+            else {
+                if($currentSegment=="")    {}
+                //else{        get_template_part( 'content' );   }
 
             }
-            wp_reset_postdata();
-
-        }
-        
-        //get_template_part( 'portfolio-wedding' );
-    }
-    if($currentSegment=='portfolio-production')   {
-        if ( $q2->have_posts() ) {
-            while ( $q2->have_posts() ) {
-                $q2->the_post();
-                // );
-                $category=get_the_category();
-                $category_use=$category[0]->term_id;
-                $postdate=   get_the_date('d') .'|'.get_the_date('m') ;
-                $post_title=mb_strimwidth(get_the_title(),0,15);
-                $post_thumbnail=get_the_post_thumbnail_url(get_the_ID(),'post_thumbnail');
-               echo'
-                <div class="col-md-6 col-xs-12 col-sm-12 col-lg-4">
-                <a href='.get_home_url().'/video/'.get_the_ID() .'/'.$category_use.'">'.'
-                <img src="'.$post_thumbnail.'">
-                '.'</a>'.'
-            <div class="video-thumb-caption-box"">
-              <div class="video-thumb-title-text" >'.$post_title .'</div>
-              <div class="video-thumb-title-date" >'.$postdate.'</div>
-            </div>
-        </div>';
-                // Your loop
-
-            }
-            wp_reset_postdata();
-        }
-        //get_template_part( 'portfolio-production' );
-    }
-    if($currentSegment=='video')   {
-        //get_template_part( 'video' );
-        hm_get_template_part( 'video',['option'=>$idseg]);
-    }
-    if($currentSegment=='thankyou')   {
-        //$ty=1;$mailcontent=   get_template_part( 'content' );
-    }
-}
- else {
-    if($currentSegment=="")    {}
-    //else{        get_template_part( 'content' );   }
-
- }
-?>
+            ?>
+        </div>
     </div>
 </div>
     <?php if(in_array($currentSegment, [$portfolio_wedding, $portfolio_production])) {?>
-    <div class="row">
-        <div>
-            <form>
-                <input type="text" name="s" style="width:200px;height:30px;opacity:0.45;background-color:gray;border:none;display:inline-block"> <span class="glyphicon glyphicon-search" style="color:white"></span>
-             </form>
+        <div class="search-field" data-style="display: flex">
+            <?php if(($currentSegment=='portfolio-wedding')||($currentSegment=='portfolio-production')){?>
+                <div class="prev-video-thumbs-arrow">
+                    <?php previous_posts_link('<i class="fa fa-4x fa-chevron-left"></i>', $q->max_num_pages); ?>
+                </div>
+            <?php }?>
+                <form>
+                    <input type="text" name="s"><span class="glyphicon glyphicon-search"></span>
+                </form>
+            <?php if(($currentSegment=='portfolio-wedding')||($currentSegment=='portfolio-production')){?>
+                <div class="next-video-thumbs-arrow">
+                    <?php next_posts_link('<i class="fa fa-4x fa-chevron-right"></i>',$q->max_num_pages);?>
+                </div>
+            <?php }?>
         </div>
-    </div>
     <?php }?>
     </div>
 <footer>
   <?= \liberty\widgets\SocialButtons::renderItems([
-        'class' => 'social-connect social-connect-one-screen-layout hidden-lg'
+        'class' => 'social-connect social-connect-one-screen-layout'
     ]) ; ?>
 </footer>
 <script src="<?php echo get_template_directory_uri(); ?>/bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
@@ -258,6 +268,18 @@ if($currentSegment=='portfolio-wedding' || $currentSegment=='portfolio-productio
 <!--        };-->
 <!--    });-->
 <!--</script>-->
+<script>
+    $(document).ready(function () {
+        var prevArrow = $('.search-field .prev-video-thumbs-arrow');
+        var nextArrow = $('.search-field .next-video-thumbs-arrow');
+        if (prevArrow.html().length < 100) {
+            prevArrow.html('<i style="opacity:0" class="fa fa-4x fa-chevron-left"></i>');
+        }
+        if (nextArrow.html().length < 100) {
+            nextArrow.html('<i style="opacity:0" class="fa fa-4x fa-chevron-right"></i>');
+        }
+    })
+</script>
 <script src="<?php echo get_template_directory_uri(); ?>/bower_components/bootstrap/dist/js/bootstrap.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/anims.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/send-text-input.js" type="text/javascript"></script>
